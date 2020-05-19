2143ea80a4a6976646dae71a012c0d43a2d082ac
Selbin_C_R <selbinzr@gmail.com>
2020-05-19 19:23:26 +0530

    changed route methods

---
+++ routes/post.route.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const router = express.Router()
+
+const postHandler = require('../controller/postHandler')
+
+// get user posts
+
+router.post('/:userId', postHandler.getUserPosts)
+
+// posting
+
+router.post('/', postHandler.createPost)
+
+// like posts
+
+router.post('/like', postHandler.like)
+
+// dislike posts
+
+router.delete('/unlike', postHandler.dislike)
+
+// comment posts
+
+router.post('/comment', postHandler.commentPost)
+
+// delete comment
+
+router.delete('/comment', postHandler.deleteComment)
+
+// get comments
+
+router.post('/getcomment', postHandler.getComments)
+
+// display timeline
+
+router.get('/timeline', postHandler.loadTimeline)
+
+module.exports = router
