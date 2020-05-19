15193ff7cfbc5aeeec4165a63e4e2ce4a9185578
Selbin_C_R <selbinzr@gmail.com>
2020-05-19 19:29:39 +0530

    added register validation

---
+++ middlewares/registerValidation.js
@@ -0,0 +1,24 @@
+const exeQuery = require('../models/db')
+
+const emailExist = async (req, res, next) => {
+  const email = req.body.email
+  try {
+    const result = await exeQuery('select * from users where email_address = $1', [email])
+    result.rowCount > 0 ? req.email = true : req.email = false
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
+const userExist = async (req, res, next) => {
+  const userName = req.body.userName
+  try {
+    const result = await exeQuery('select * from users where username = $1', [userName])
+    result.rowCount > 0 ? req.user = true : req.user = false
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+module.exports = { emailExist, userExist }
