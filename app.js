0335be3aadb600fa3f95fc6912343d66be383aab
Selbin_C_R <selbinzr@gmail.com>
2020-05-19 19:24:24 +0530

    root file

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const indexRoutes = require('./routes/index.route')
+const dotEnv = require('dotenv')
+const authenticate = require('./middlewares/auth')
+const cors = require('cors')
+
+const app = express()
+dotEnv.config()
+
+if (!process.env.PRIVATEKEY) process.exit(1)
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.static('static'))
+app.use(authenticate)
+
+app.use('/picter/api', indexRoutes)
+
+const port = process.env.PORT || 6000
+app.listen(port, () => console.log(`Listening on port ${port}...`))
