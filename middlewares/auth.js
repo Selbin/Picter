75c31354e0efe2ea391a16e298ce8c331a46ab84
Selbin_C_R <selbinzr@gmail.com>
2020-05-24 16:40:05 +0530

    removd unwanted consoles

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,10 @@
 const jwt = require('jsonwebtoken')
 
 const authenticate = (req, res, next) => {
-  if (req.url === '/api/picter/user/register' || req.url === '/api/picter/user/login') return next()
+  if (req.url === '/api/picter/auth/register' || req.url === '/api/picter/auth/login') return next()
   const token = req.headers['x-auth-token']
   if (!token) return res.status(401).json({ message: 'please login' })
   jwt.verify(token, process.env.PRIVATEKEY, (err, user) => {
-    console.log('Authenticate part')
     if (err) return res.status(401).json({ message: 'invalid user' })
     req.user = user
     next()
