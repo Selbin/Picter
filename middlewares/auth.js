89f620884719acfe1dda10227b4be8ab59c5cefb
Selbin_C_R <selbinzr@gmail.com>
2020-05-24 19:05:10 +0530

    removed check from authentication

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 const authenticate = (req, res, next) => {
-  if (req.url === '/api/picter/auth/register' || req.url === '/api/picter/auth/login') return next()
+  if (req.url === '/api/picter/auth/register' || req.url === '/api/picter/auth/login' || req.url === '/api/picter/auth/check') return next()
   const token = req.headers['x-auth-token']
   if (!token) return res.status(401).json({ message: 'please login' })
   jwt.verify(token, process.env.PRIVATEKEY, (err, user) => {
