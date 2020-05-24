e5a9ac5cfc112ecc97821c8cfaf97a08bbef8d80
Selbin_C_R <selbinzr@gmail.com>
2020-05-24 16:40:48 +0530

    added new route path

--- routes/index.route.js
+++ routes/index.route.js
@@ -3,9 +3,11 @@ const router = express.Router()
 const userRouter = require('./user.route')
 const postRouter = require('./post.route')
 const imageRouter = require('./image.route')
+const authRouter = require('./auth.route')
 
 router.use('/user', userRouter)
 router.use('/post', postRouter)
 router.use('/image', imageRouter)
+router.use('/auth', authRouter)
 
 module.exports = router
