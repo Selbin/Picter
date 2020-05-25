5bc111881878c17565fb49a5596987a10d21112e
Selbin_C_R <selbinzr@gmail.com>
2020-05-25 10:42:25 +0530

    updated queries

--- picterDb.sql
+++ picterDb.sql
@@ -1,31 +1,4 @@
-CREATE TABLE comments 
-(
-  comment_id SERIAL PRIMARY KEY,
-  post_id INTEGER NOT NULL,
-  user_id INTEGER NOT NULL,
-  comment TEXT NOT NULL,
-  commented_on VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE likes
-(
-  like_id SERIAL PRIMARY KEY,
-  post_id INTEGER NOT NULL,
-  user_id INTEGER NOT NULL,
-  liked_on VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE posts
-(
-  post_id SERIAL PRIMARY KEY,
-  caption TEXT DEFAULT '', 
-  image_urls TEXT [] NOT NULL,
-  posted_by INTEGER NOT NULL,
-  posted_on VARCHAR(255) NOT NULL,
-  like_count INTEGER DEFAULT 0,
-  comment_count INTEGER DEFAULT 0
-);
-
+ 
 CREATE TABLE users
 (
   user_id SERIAL PRIMARY KEY,
@@ -34,8 +7,6 @@ CREATE TABLE users
   last_name VARCHAR(100) NOT NULL,
   email_address VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  gender VARCHAR(6) NOT NULL,
-  city VARCHAR(100) DEFAULT '',
   bio TEXT DEFAULT '',
   profile_pic TEXT DEFAULT '',
   followers INTEGER DEFAULT 0,
@@ -43,9 +14,38 @@ CREATE TABLE users
   registered_on VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE followers (
+CREATE TABLE posts
+(
+  post_id SERIAL PRIMARY KEY,
+  caption TEXT DEFAULT '', 
+  image_urls TEXT [] NOT NULL,
+  posted_by INTEGER NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+  posted_on VARCHAR(255) NOT NULL,
+  like_count INTEGER DEFAULT 0,
+  comment_count INTEGER DEFAULT 0
+);
+
+CREATE TABLE likes
+(
+  like_id SERIAL PRIMARY KEY,
+  post_id INTEGER NOT NULL REFERENCES posts(post_id) ON DELETE CASCADE,
+  user_id INTEGER NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+  liked_on VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE comments 
+(
+  comment_id SERIAL PRIMARY KEY,
+  post_id INTEGER NOT NULL REFERENCES posts(post_id) ON DELETE CASCADE,
+  user_id INTEGER NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+  comment TEXT NOT NULL,
+  commented_on VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE followers
+(
   follower_id SERIAL PRIMARY KEY,
-  follower_user INTEGER NOT NULL,
-  following_user INTEGER NOT NULL,
+  follower_user INTEGER NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+  following_user INTEGER NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
   followed_on VARCHAR(255) NOT NULL
 );
